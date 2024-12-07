a85f9586f0fb5e3dec1c82a643efb614f82149a5
Leo Villalobos <villaleobos@gmail.com>
2024-12-06 16:23:53 -0800

    feat: add favorite memory in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 loadHeroSection();
 
 const testimonialSectionArgs = {
-  paragraphText: "One of my favorite memories in Salinas is when...",
+  paragraphText: "I remember when I was a kid, going to Salinas was always a big deal. My mom would dress me and my brothers and we would head to the mall. I especially rememeber the back to school shopping we would do. There also used to be a carasouel in the center of the mall and I remember I always wanted to ride it.",
   videoURI: "/public/videos/sample-video.mp4",
 };
 
@@ -10,7 +10,7 @@ loadTestimonialSection(testimonialSectionArgs);
 function loadHeroSection() {
   const overviewSection = document.getElementById("salinas-overview");
   const overviewImageURI = "/public/images/salinas-mountains.jpg";
-  
+
   overviewSection.style.background = `
   linear-gradient(var(--dnest-opaque-magenta), transparent),
     center no-repeat url(${overviewImageURI})
